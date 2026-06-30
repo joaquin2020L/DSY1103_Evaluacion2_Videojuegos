15097e941aa1e54b860d8534280a65c85dd869d3
Joaquin Lagos <joaq.lagos@duocuc.cl>
2026-06-29 22:50:43 -0400

    Implementacion de YAML, Swagger y Pruebas Unitarias EP3

--- usuarios/src/main/java/com/tienda/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/com/tienda/usuarios/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.tienda.usuarios.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import com.tienda.usuarios.model.Usuario;
 import com.tienda.usuarios.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/api/usuarios")
+@Tag(name = "Gestión de Usuarios y Roles", description = "Endpoints para la autenticación y registro de clientes en GameStore")
 public class UsuarioController {
 
     @Autowired
@@ -25,8 +30,11 @@ public class UsuarioController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-
+@Operation(summary = "Registrar un nuevo usuario", description = "Crea un cliente en el sistema asignándole un rol por defecto.")
+    @ApiResponse(responseCode = "201", description = "Usuario creado con éxito")
+    @ApiResponse(responseCode = "400", description = "Error en las validaciones o el correo ya existe")
     @PostMapping("/login")
+    
     public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
         String token = usuarioService.login(email, password);
         if (token.contains("TOKEN")) {
